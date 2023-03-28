f625315cdfe13fd3f5b08a6260e666aa8fa99380
Nabil Es-salihi <essalihinabil@gmail.com>
2023-03-28 03:11:06 +0200

    Sorted alphabeta

--- src/strategy/alphabeta.rs
+++ src/strategy/alphabeta.rs
@@ -4,6 +4,7 @@ use std::fmt;
 use super::Strategy;
 use crate::configuration::{Configuration, Movement};
 use crate::shmem::AtomicMove;
+use std::time::Instant;
 
 /// Anytime alpha beta algorithm.
 /// Any time algorithms will compute until a deadline is hit and the process is killed.
@@ -27,7 +28,7 @@ impl fmt::Display for AlphaBeta {
 }
 
 fn alpha_beta(depth: u8, state: &Configuration, mut alpha: i8, beta: i8) -> (i8, Option<Movement>) {
-    if depth == 0 || state.game_over() {
+    if depth == 0 || !state.can_move() {
         return (state.value(), None);
     }
     let mut best_score = i8::MIN;
@@ -46,16 +47,42 @@ fn alpha_beta(depth: u8, state: &Configuration, mut alpha: i8, beta: i8) -> (i8,
             }
         }
     }
-    if best_score == -128 {
-        best_score = state.value();
-        best_move = None;
+    assert_ne!(best_score, i8::MIN);
+    (-best_score, best_move)
+}
+
+fn alpha_beta_sorted(depth: u8, state: &Configuration, mut alpha: i8, beta: i8) -> (i8, Option<Movement>) {
+    if depth == 0 || !state.can_move() {
+        return (state.value(), None);
     }
+    let mut best_score = i8::MIN;
+    let mut best_move: Option<Movement> = None;
+    let mut sorted_moves: Vec<Movement> = state.movements().collect::<Vec<Movement>>();
+    sorted_moves.sort_by_key(|m| -state.play(m).value());
+    for movement in sorted_moves.into_iter() {
+        let next_conf : Configuration = state.play(&movement);
+        let (score, _) = alpha_beta_sorted(depth - 1, &next_conf, -beta, -alpha);
+        if score > best_score {
+            best_score = score;
+            best_move = Some(movement);
+            if best_score > alpha {
+                alpha = best_score;
+            }
+            if beta < alpha {
+                break;
+            }
+        }
+    }
+    assert_ne!(best_score, i8::MIN);
     (-best_score, best_move)
 }
 
 impl Strategy for AlphaBeta {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        let (_, mv) = alpha_beta(self.0, state, i8::MIN + 1, i8::MAX);
+        let start_time = Instant::now();
+        let (_, mv) = alpha_beta_sorted(self.0, state, i8::MIN + 1, i8::MAX);
+        let end_time = Instant::now();
+        println!("Time elapsed: {:?}", end_time.duration_since(start_time));
         mv
     }
 }
